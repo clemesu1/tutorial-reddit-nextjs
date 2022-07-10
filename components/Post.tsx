06576cc5e30cc5638e852f1e54a5feae12e29f6c
clemesu1 <clemesu1@unb.ca>
2022-07-10 12:11:36 -0300

    implemented voting functionality

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   ArrowDownIcon,
   ArrowUpIcon,
@@ -12,12 +12,74 @@ import Avatar from "./Avatar";
 import TimeAgo from "react-timeago";
 import Link from "next/link";
 import { Jelly } from "@uiball/loaders";
+import { useSession } from "next-auth/react";
+import toast from "react-hot-toast";
+import { useMutation, useQuery } from "@apollo/client";
+import { GET_ALL_VOTES_BY_POST_ID } from "../graphql/queries";
+import { ADD_VOTE } from "../graphql/mutations";
 
 type Props = {
   post: Post;
 };
 
 function Post({ post }: Props) {
+  const [vote, setVote] = useState<boolean>();
+  const { data: session } = useSession();
+  const { data, loading } = useQuery(GET_ALL_VOTES_BY_POST_ID, {
+    variables: {
+      post_id: post?.id,
+    },
+  });
+
+  const [addVote] = useMutation(ADD_VOTE, {
+    refetchQueries: [GET_ALL_VOTES_BY_POST_ID, "getVotesByPostId"],
+  });
+
+  const handleVote = async (isUpvote: boolean) => {
+    if (!session) {
+      toast("❗You'll need to sign in to vote!");
+      return;
+    }
+
+    if (vote && isUpvote) return;
+    if (vote === false && !isUpvote) return;
+
+    console.log("voting...", isUpvote);
+
+    await addVote({
+      variables: {
+        post_id: post.id,
+        username: session?.user?.name,
+        upvote: isUpvote,
+      },
+    });
+  };
+
+  const displayVotes = (data: any) => {
+    const votes: Vote[] = data?.getVotesByPostId;
+    const displayNumber = votes?.reduce(
+      (total, vote) => (vote.upvote ? (total += 1) : (total -= 1)),
+      0,
+    );
+
+    if (votes?.length === 0) return 0;
+    if (displayNumber === 0) {
+      return votes[0]?.upvote ? 1 : -1;
+    }
+
+    return displayNumber;
+  };
+
+  useEffect(() => {
+    const votes: Vote[] = data?.getVotesByPostId;
+
+    const vote = votes?.find(
+      (vote) => vote.username == session?.user?.name,
+    )?.upvote;
+
+    setVote(vote);
+  }, [data]);
+
   if (!post)
     return (
       <div className="flex items-center justify-center w-full p-10 text-xl">
@@ -29,9 +91,19 @@ function Post({ post }: Props) {
       <div className="flex bg-white border border-gray-300 rounded-md shadow-sm cursor-pointer hover:border hover:border-gray-600">
         {/* Votes */}
         <div className="flex flex-col items-center justify-start p-4 space-y-1 text-gray-400 rounded-l-md bg-gray-50">
-          <ArrowUpIcon className="voteButton hover:text-red-400" />
-          <p className="text-xs font-bold text-black">{0}</p>
-          <ArrowDownIcon className="voteButton hover:text-blue-400" />
+          <ArrowUpIcon
+            onClick={() => handleVote(true)}
+            className={`voteButton hover:text-red-400 ${
+              vote && "text-red-400"
+            }`}
+          />
+          <p className="text-xs font-bold text-black">{displayVotes(data)}</p>
+          <ArrowDownIcon
+            onClick={() => handleVote(false)}
+            className={`voteButton hover:text-blue-400 ${
+              vote === false && "text-blue-400"
+            }`}
+          />
         </div>
 
         <div className="p-3 pb-1">
