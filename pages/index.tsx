59a63c9d9d3cf877e495af137598b4a789cdbbfb
clemesu1 <clemesu1@unb.ca>
2022-07-10 10:10:03 -0300

    implemented feed section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Feed from "../components/Feed";
 import PostBox from "../components/PostBox";
 
 const Home: NextPage = () => {
@@ -10,7 +11,9 @@ const Home: NextPage = () => {
       </Head>
 
       <PostBox />
-      <div className="flex"></div>
+      <div className="flex">
+        <Feed />
+      </div>
     </div>
   );
 };
